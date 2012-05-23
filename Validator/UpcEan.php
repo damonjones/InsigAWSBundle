cbf524d6c78388873a554337718ec7d17022834e
Damon Jones <damon@insig.net>
2012-05-23 17:39:20 -0400

    Added short licenses to each php file

--- Validator/UpcEan.php
+++ Validator/UpcEan.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the InsigAWSBundle package.
+ *
+ * (c) Damon Jones <damon@insig.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Insig\AWSBundle\Validator;
 
 use Symfony\Component\Validator\Constraint;
